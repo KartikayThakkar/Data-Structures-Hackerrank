37cd4b25c819aa9f3555e265442fa87a2aa9ea09
KartikayThakkar <kartikaythakkar@gmail.com>
2017-11-15 16:13:04 +0530

    Add files via upload

---
+++ Arrays Challenges/2DArray.c
@@ -0,0 +1,102 @@
+/*
+Given a  2D Array, :
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+We define an hourglass in  to be a subset of values with indices falling in this pattern in 's graphical representation:
+
+a b c
+  d
+e f g
+There are  hourglasses in , and an hourglass sum is the sum of an hourglass' values.
+
+Task 
+Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum.
+
+Note: If you have already solved the Java domain's Java 2D Array challenge, you may wish to skip this challenge.
+
+Input Format
+
+There are  lines of input, where each line contains  space-separated integers describing 2D Array ; every value in  will be in the inclusive range of  to .
+
+Constraints
+
+Output Format
+
+Print the largest (maximum) hourglass sum found in .
+
+Sample Input
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+Sample Output
+
+19
+Explanation
+
+ contains the following hourglasses:
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  1       0       0       0
+1 1 1   1 1 0   1 0 0   0 0 0
+
+0 1 0   1 0 0   0 0 0   0 0 0
+  1       1       0       0
+0 0 2   0 2 4   2 4 4   4 4 0
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  0       2       4       4
+0 0 0   0 0 2   0 2 0   2 0 0
+
+0 0 2   0 2 4   2 4 4   4 4 0
+  0       0       2       0
+0 0 1   0 1 2   1 2 4   2 4 0
+The hourglass with the maximum sum () is:
+
+2 4 4
+  2
+1 2 4
+*/
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int a[6][6],k=0,b[16],i,j;
+        for(i=0;i<6;i++){
+                for(j=0;j<6;j++){
+                        scanf("%d",&a[i][j]);
+                }
+        }
+        
+        for(i=1;i<5;i++){
+                for(j=1;j<5;j++){
+                        b[k]=a[i-1][j-1]+a[i-1][j]+a[i-1][j+1]+
+                                a[i][j]+
+                                a[i+1][j-1]+a[i+1][j]+a[i+1][j+1];
+                        k++;
+                }
+        }
+        
+        int largest=b[0];
+        for(int g=1;g<k;g++){
+                if(b[g]>largest){
+                        largest=b[g];
+                }
+        }
+        printf("%d",largest);
+    return 0;
+}
